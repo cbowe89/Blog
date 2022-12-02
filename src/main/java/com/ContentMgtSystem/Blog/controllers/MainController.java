f17a7752006fc5821656926d8c45e04fe4bce63d
Courtney Bowe <csmyers89@gmail.com>
2022-12-02 13:40:44 -0600

    Added Need_To_Do.txt

--- src/main/java/com/ContentMgtSystem/Blog/controllers/MainController.java
+++ src/main/java/com/ContentMgtSystem/Blog/controllers/MainController.java
@@ -1,9 +1,6 @@
 package com.ContentMgtSystem.Blog.controllers;
 
-import com.ContentMgtSystem.Blog.repositories.PostRepository;
-import com.ContentMgtSystem.Blog.repositories.RoleRepository;
-import com.ContentMgtSystem.Blog.repositories.TagRepository;
-import com.ContentMgtSystem.Blog.repositories.UserRepository;
+import com.ContentMgtSystem.Blog.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @Autowired
+    Post_StatusRepository post_statusRepository;
+
     @Autowired
     PostRepository postRepository;
 
