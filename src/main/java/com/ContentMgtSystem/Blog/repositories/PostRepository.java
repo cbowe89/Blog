c86ebb7fe0f86293fc40109ee63245fb19f2b048
yareenm <yareenm@outlook.com>
2022-12-02 23:25:06 -0600

    Updated findAllByTimestamp function

--- src/main/java/com/ContentMgtSystem/Blog/repositories/PostRepository.java
+++ src/main/java/com/ContentMgtSystem/Blog/repositories/PostRepository.java
@@ -14,8 +14,16 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByUser(User user);
 
     // Get the most recent post based on Timestamp that has approved status
-    @Query(value = "SELECT * FROM post WHERE status_name = 'approved' ORDER BY created_date DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT p.* from post AS p JOIN post_status AS ps" +
+            " ON p.status_id = ps.status_id" +
+            " WHERE ps.status_name = 'approved' ORDER BY created_date DESC LIMIT 1", nativeQuery = true)
     List<Post> findAllByTimestamp();
+
+
+    // Get all posts in descending order based on Timestamp
+    @Query(value = "SELECT * FROM post WHERE status_id = 2 ORDER BY created_date DESC",
+            nativeQuery = true)
+    List<Post> findAllByOrderByCreated_dateDesc();
 
     // display the pending posts
     @Query(value = "SELECT p.* from post AS p JOIN post_status AS ps" +
