c3431debbe7c4d08ecae0816a7095ccc3cdd4e66
Courtney Bowe <csmyers89@gmail.com>
2022-12-08 20:37:20 -0600

    Cleaning up imports/dependencies

--- src/main/java/com/ContentMgtSystem/Blog/entities/Post.java
+++ src/main/java/com/ContentMgtSystem/Blog/entities/Post.java
@@ -2,7 +2,6 @@ package com.ContentMgtSystem.Blog.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.sql.Timestamp;
